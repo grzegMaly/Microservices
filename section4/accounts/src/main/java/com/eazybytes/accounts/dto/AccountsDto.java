b07e9de27fe6ecc660c74a3ef10becf024a2d964
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-03 19:07:52 +0100

    Section4-Accounts Code

---
+++ section4/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -0,0 +1,27 @@
+package com.eazybytes.accounts.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
+public class AccountsDto {
+
+    @Schema(description = "Account Number of Bank account", example = "1231231231")
+    @NotEmpty(message = "AccountNumber can not be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 digits")
+    private Long accountNumber;
+
+    @Schema(description = "Account type of Bank account", example = "Savings")
+    @NotEmpty(message = "AccountType can not be null or empty")
+    private String accountType;
+
+    @Schema(description = "Bank Branch address", example = "123 New York")
+    @NotEmpty(message = "BranchAddress can not be null or empty")
+    private String branchAddress;
+}
