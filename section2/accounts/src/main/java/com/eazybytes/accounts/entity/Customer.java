36046bb49b11732d31fc89826e41acf6d34f6834
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-03 18:41:17 +0100

    Section2-Accounts Code

---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/entity/Customer.java
@@ -0,0 +1,23 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private Long customerId;
+    private String name;
+    private String email;
+    private String mobileNumber;
+}
