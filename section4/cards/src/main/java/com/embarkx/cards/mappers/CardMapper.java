eb86acef72f239f3dec255b68345a405c5dc77a4
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-03 19:08:11 +0100

    Section4-Card Code

---
+++ section4/cards/src/main/java/com/embarkx/cards/mappers/CardMapper.java
@@ -0,0 +1,29 @@
+package com.embarkx.cards.mappers;
+
+import com.embarkx.cards.dto.CardDto;
+import com.embarkx.cards.entity.Card;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CardMapper {
+
+    public CardDto mapToCardDto(Card card, CardDto cardDto) {
+        cardDto.setCardNumber(card.getCardNumber());
+        cardDto.setCardType(card.getCardType());
+        cardDto.setMobileNumber(card.getMobileNumber());
+        cardDto.setTotalLimit(card.getTotalLimit());
+        cardDto.setAvailableAmount(card.getAvailableAmount());
+        cardDto.setAmountUsed(card.getAmountUsed());
+        return cardDto;
+    }
+
+    public Card mapToCard(CardDto cardDto, Card card) {
+        card.setCardNumber(cardDto.getCardNumber());
+        card.setCardType(cardDto.getCardType());
+        card.setMobileNumber(cardDto.getMobileNumber());
+        card.setTotalLimit(cardDto.getTotalLimit());
+        card.setAvailableAmount(cardDto.getAvailableAmount());
+        card.setAmountUsed(cardDto.getAmountUsed());
+        return card;
+    }
+}
---
+++ section4/cards/src/main/java/com/embarkx/cards/services/CardServiceImpl.java
@@ -0,0 +1,69 @@
+package com.embarkx.cards.services;
+
+import com.embarkx.cards.constants.CardsConstants;
+import com.embarkx.cards.dto.CardDto;
+import com.embarkx.cards.entity.Card;
+import com.embarkx.cards.exceptions.CardAlreadyExistsException;
+import com.embarkx.cards.exceptions.ResourceNotFoundException;
+import com.embarkx.cards.mappers.CardMapper;
+import com.embarkx.cards.repositories.CardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class CardServiceImpl implements CardService {
+
+    private final CardRepository cardRepository;
+    private final CardMapper cardMapper;
+
+    @Override
+    public void createCard(String mobileNumber) {
+        Optional<Card> optionalCard = cardRepository.findByMobileNumber(mobileNumber);
+        if (optionalCard.isPresent()) {
+            throw new CardAlreadyExistsException("Card already registered with given mobileNumber " + mobileNumber);
+        }
+        cardRepository.save(createNewCard(mobileNumber));
+    }
+
+    private Card createNewCard(String mobileNumber) {
+        Card newCard = new Card();
+        long randomCardNumber = 100000000000L + new Random().nextInt(900000000);
+        newCard.setCardNumber(Long.toString(randomCardNumber));
+        newCard.setMobileNumber(mobileNumber);
+        newCard.setCardType(CardsConstants.CREDIT_CARD);
+        newCard.setTotalLimit(CardsConstants.NEW_CARD_LIMIT);
+        newCard.setAmountUsed(0);
+        newCard.setAvailableAmount(CardsConstants.NEW_CARD_LIMIT);
+        return newCard;
+    }
+
+    @Override
+    public CardDto fetchCard(String mobileNumber) {
+        Card cards = cardRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Card", "mobileNumber", mobileNumber)
+        );
+        return cardMapper.mapToCardDto(cards, new CardDto());
+    }
+
+    @Override
+    public boolean updateCard(CardDto cardDto) {
+        Card card = cardRepository.findByCardNumber(cardDto.getCardNumber()).orElseThrow(
+                () -> new ResourceNotFoundException("Card", "CardNumber", cardDto.getCardNumber()));
+        cardMapper.mapToCard(cardDto, card);
+        cardRepository.save(card);
+        return true;
+    }
+
+    @Override
+    public boolean deleteCard(String mobileNumber) {
+        Card card = cardRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Card", "mobileNumber", mobileNumber)
+        );
+        cardRepository.deleteById(card.getCardId());
+        return true;
+    }
+}
