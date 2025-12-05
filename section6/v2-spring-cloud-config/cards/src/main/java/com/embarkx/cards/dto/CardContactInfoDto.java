34dc72f8e06177a69043280e77458527ef95e38b
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-05 13:44:51 +0100

    Section6-Cloud Github Config for Cards

--- section6/v2-spring-cloud-config/cards/src/main/java/com/embarkx/cards/dto/CardContactInfoDto.java
+++ section6/v2-spring-cloud-config/cards/src/main/java/com/embarkx/cards/dto/CardContactInfoDto.java
@@ -1,10 +1,19 @@
 package com.embarkx.cards.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @ConfigurationProperties(prefix = "cards")
-public record CardContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class CardContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
