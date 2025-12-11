ce5c69d5462fde7e170228777aace91ef536110b
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-11 17:03:48 +0100

    Section9-Loans Code

---
+++ section9/loans/src/main/java/com/embarkx/loans/mappers/LoanMapper.java
@@ -0,0 +1,29 @@
+package com.embarkx.loans.mappers;
+
+import com.embarkx.loans.dto.LoansDto;
+import com.embarkx.loans.entity.Loans;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoanMapper {
+
+    public Loans mapToLoan(LoansDto loansDto, Loans loans) {
+        loans.setLoanNumber(loansDto.getLoanNumber());
+        loans.setLoanType(loansDto.getLoanType());
+        loans.setMobileNumber(loansDto.getMobileNumber());
+        loans.setTotalLoan(loansDto.getTotalLoan());
+        loans.setAmountPaid(loansDto.getAmountPaid());
+        loans.setOutstandingAmount(loansDto.getOutstandingAmount());
+        return loans;
+    }
+
+    public LoansDto mapToLoanDto(Loans loans, LoansDto loansDto) {
+        loansDto.setLoanNumber(loans.getLoanNumber());
+        loansDto.setLoanType(loans.getLoanType());
+        loansDto.setMobileNumber(loans.getMobileNumber());
+        loansDto.setTotalLoan(loans.getTotalLoan());
+        loansDto.setAmountPaid(loans.getAmountPaid());
+        loansDto.setOutstandingAmount(loans.getOutstandingAmount());
+        return loansDto;
+    }
+}
