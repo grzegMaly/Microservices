8bc723cbb31d4954d91a5d4a61374c05af07d916
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-15 15:58:00 +0100

    Section12-Security Code

--- section12/gatewayserver/src/main/java/com/embarkx/gatewayserver/config/KeycloakRoleConverter.java
+++ section12/gatewayserver/src/main/java/com/embarkx/gatewayserver/config/KeycloakRoleConverter.java
@@ -1,4 +1,32 @@
 package com.embarkx.gatewayserver.config;
 
-public class KeycloakRoleConverter {
+import lombok.NonNull;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(@NonNull Jwt source) {
+
+        if (!(source.getClaims().get("realm_access") instanceof Map<?, ?> rA)
+                || rA.isEmpty() || !(rA.get("roles") instanceof List<?> rV)) {
+            return new ArrayList<>();
+        }
+
+        return rV.stream()
+                .filter(String.class::isInstance)
+                .map(String.class::cast)
+                .map("ROLE_"::concat)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
 }
