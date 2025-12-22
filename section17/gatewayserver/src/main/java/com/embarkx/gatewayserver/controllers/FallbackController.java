8746c7c57bfb4399e0db99d4656dd4e409194e25
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-22 14:46:25 +0100

    Section17-Gateway kubernetes discoveryserver

---
+++ section17/gatewayserver/src/main/java/com/embarkx/gatewayserver/controllers/FallbackController.java
@@ -0,0 +1,15 @@
+package com.embarkx.gatewayserver.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/contactSupport")
+    public Mono<String> contactSupport() {
+        return Mono.just("An error occurred. Please try after some time or contact support team!!");
+    }
+}
