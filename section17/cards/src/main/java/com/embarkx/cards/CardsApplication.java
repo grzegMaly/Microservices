8069d5a08c3375c1725209534574da6cfea939fe
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-22 14:47:08 +0100

    Section17-Cards kubernetes discoveryserver

---
+++ section17/cards/src/main/java/com/embarkx/cards/CardsApplication.java
@@ -0,0 +1,46 @@
+package com.embarkx.cards;
+
+import com.embarkx.cards.dto.CardContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Card service REST API Documentation",
+                description = "Card microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Mordinia Mordinia",
+                        email = "mordinia@mordinia.com",
+                        url = "mordinia.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.mordinia.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Card Microservice REST API Documentation",
+                url = "https://www.mordinia.com/swagger-ui.html"
+        )
+)
+@EnableConfigurationProperties(value = {CardContactInfoDto.class})
+@EnableDiscoveryClient
+public class CardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardsApplication.class, args);
+    }
+
+}
