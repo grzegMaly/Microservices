6e71f28692467d5984d168318110dc3d4686531b
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-23 12:32:37 +0100

    Section20-Accounts BOM

---
+++ section20/accounts/src/main/java/com/eazybytes/accounts/client/CardsFeignClient.java
@@ -0,0 +1,18 @@
+package com.eazybytes.accounts.client;
+
+import com.eazybytes.accounts.dto.CardDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "cards", fallback = CardsFallback.class)
+public interface CardsFeignClient {
+
+    @GetMapping(value = "/api/fetch", consumes = "application/json")
+    ResponseEntity<CardDto> fetchCardDetails(
+            @RequestHeader("bank-correlation-id") String correlationId,
+            @RequestParam String mobileNumber
+    );
+}
---
+++ section20/accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
@@ -0,0 +1,61 @@
+package com.eazybytes.accounts.controller;
+
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+import com.eazybytes.accounts.services.CustomerService;
+import com.eazybytes.common.dto.ErrorResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Pattern;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@Validated
+@RestController
+@RequiredArgsConstructor
+@Tag(
+        name = "CRUD REST APIs for Customers in Bank",
+        description = "CRUD REST APIs int Bank to FETCH customer details"
+)
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Operation(
+            summary = "Fetch Customer Details REST API",
+            description = "REST API to fetch Customer details based on a mobile number")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    ))
+    })
+    @GetMapping("/fetchCustomerDetails")
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(
+            @RequestHeader("bank-correlation-id") String correlationId,
+            @RequestParam
+            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+            String mobileNumber
+    ) {
+        log.debug("fetchCustomerDetails method start");
+        CustomerDetailsDto customerDetailsDto = customerService.fetchCustomerDetails(mobileNumber, correlationId);
+        log.debug("fetchCustomerDetails method end");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(customerDetailsDto);
+    }
+}
