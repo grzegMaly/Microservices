8a75e2fd4b0773eff12aea9faf2002d2a473abc7
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-24 12:09:25 +0100

    Section12-Scripts Update

---
+++ section12/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+services=(accounts cards configserver eurekaserver gatewayserver loans)
+
+for s in "${services[@]}"; do
+  echo "Building $s"
+  (cd "$s" && mvn compile jib:dockerBuild)
+done
+
+for s in "${services[@]}"; do
+  echo "Pushing $s"
+  docker push "mordiniaa/$s:s12"
+done
+
+./start.sh
