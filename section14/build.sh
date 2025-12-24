b56a2444cecff5c36aa9ae1573c12e23baa6cbe4
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-24 12:09:48 +0100

    Section14-Scripts Update

---
+++ section14/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+services=(accounts cards configserver eurekaserver gatewayserver loans message)
+
+for s in "${services[@]}"; do
+  echo "Building $s"
+  (cd "$s" && mvn compile jib:dockerBuild)
+done
+
+for s in "${services[@]}"; do
+  echo "Pushing $s"
+  docker push "mordiniaa/$s:s14"
+done
